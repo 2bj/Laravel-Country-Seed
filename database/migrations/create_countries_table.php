69cb49523b9229673bcc7edd355c84db2c0db65f
Stroemgren <casper@stroemgren.dk>
2014-01-16 19:41:37 +0100

    Add seed and migration file

---
+++ database/migrations/create_countries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('countries', function($table) {
+			$table->increments('id');
+			$table->string('code');
+			$table->string('name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('countries');
+	}
+
+}
